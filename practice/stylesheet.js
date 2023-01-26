7cf4d0348a947cf3a59e31d53af8875eb04d39f8
Akutsang <mymac@MYs-MacBook-Pro.local>
2023-01-26 09:25:43 +0100

    array methods to get familiar with

--- practice/stylesheet.js
+++ practice/stylesheet.js
@@ -19,3 +19,21 @@ const people = [ "Beck, Glenn", "Becker, Carl", "Beckett, Samuel", "Beddoes, Mic
 "Birell, Augustine", "Black Elk", "Blair, Robert", "Blair, Tony,", "Blake, William"]
 
 
+// Array.prototype.filter()
+// 1. Filter the list of inventors for those who were born in the 1500's
+
+// Array.prototype.map()
+// 2. Give us an array of the inventory first and last names
+
+// Array.prototype.sort()
+// 3. Sort the inventors by birthdays, oldest to youngest
+
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live?
+
+// 5. Sort the inventors by years lived
+
+// 6. Creat a list of Boulevards in Paris that contains "de" anywhere in the name 
+// https://en.wikipedia.org/wiki/category:Boulevards_in_paris
+
+
